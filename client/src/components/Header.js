e7450e55b3fdec6b4459436995d881632255565d
root <root@ip-172-31-33-9.ec2.internal>
2022-09-07 21:25:27 +0000

    red

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "white" : "orange"}
+          color={showAdd ? "red" : "red"}
           text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
